247a6f4174f58d1230598247b799b0868b827619
jhonldev <jhon.lopez.developer@gmail.com>
2023-09-14 15:47:53 -0300

    first commit

---
+++ database/migrations/2023_09_10_031145_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->date('reservation_date');
+            $table->integer('quantity_seats');
+            $table->dateTime('purchase_date');
+            $table->integer('payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+<h1>Hello world</h1>
+<h2>Esto es una prueba</h2>
+<a href="{{route('register')}}">IR a registro</a>
+</body>
+</html>
